f9c56ebf10f84523084591d48f6874a5c0a6e35d
Alex <youzhny.foto@gmail.com>
2022-08-13 18:24:39 +0300

    task done!

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
+
 class GuessingGame {
-    constructor() {}
 
-    setRange(min, max) {
-
+    constructor() {
+        this.minNum = 0;
+        this.maxNum = 0;
+        this.result = 0;
     }
 
-    guess() {
+    setRange(min, max) {
+        this.minNum = min;
+        this.maxNum = max;
+    }    
 
+    guess() {
+        this.result = Math.round(this.minNum + (this.maxNum - this.minNum)/2);
+        return this.result;
     }
 
     lower() {
-
+        this.maxNum = this.result;
     }
 
     greater() {
-
+        this.minNum = this.result;
     }
 }
+
 
 module.exports = GuessingGame;
